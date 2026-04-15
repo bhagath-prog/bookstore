9112dcc29daeb730f1df8338cf73759558ca92dd
bhagath-prog <bhagathmabc@gmail.com>
2026-04-15 17:40:52 +0530

    fix signup component

---
+++ src/components/signup.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "../components/signup";
+
+export default function SignupPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
+      <SignupForm />
+    </main>
+  );
+}
